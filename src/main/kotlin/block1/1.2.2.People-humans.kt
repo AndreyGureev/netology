c28c38ebf1170109999b5f8a47f6ca06bf0fdba9
ANDru <93010255+AndreyGureev@users.noreply.github.com>
2022-03-17 16:28:30 +0400

    Update 1.2.2.People-humans.kt
    Modification

--- src/main/kotlin/block1/1.2.2.People-humans.kt
+++ src/main/kotlin/block1/1.2.2.People-humans.kt
@@ -12,12 +12,6 @@ fun main() {
             } else {
                 println("Понравилось $likes людям")
             }
-//            when {
-//                likes.mod(100) == 11 -> println("Понравилось $likes людям")
-//               else -> {
-//                    println("Понравилось $likes человеку")
-//                }
-//            }
             if (likes < 0) {
                 println("Не верный ввод данных, введите положительное число!")
             }
