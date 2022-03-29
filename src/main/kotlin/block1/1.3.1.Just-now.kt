660b872207a318ba8bbf9821637212ef9e29152b
ANDru <93010255+AndreyGureev@users.noreply.github.com>
2022-03-29 21:36:55 +0400

    Update 1.3.1.Just-now.kt
    Revision completed

--- src/main/kotlin/block1/1.3.1.Just-now.kt
+++ src/main/kotlin/block1/1.3.1.Just-now.kt
@@ -1,78 +1,60 @@
 package block1
 
 const val MINUTE = 60
-const val HOUR = 3600
-const val DAY = 86400
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
 
 // «1.3. Управляющие конструкции. Функции»
 // Задача №1 - «Только что»
 fun main() {
     while (true) {
-        print("Введите число в минутах: ")
-        try {
-            val sum = readLine()?.toInt() ?: return
-            agoToText(sum)
-        } catch (e: Exception) {
-            println("Не верный ввод данных, введите повторно!")
+        var sum: Int
+        print("Введите число в секундах: ")
+        while (true) {
+            try {
+                sum = readLine()?.toInt() ?: return
+                if (sum < 0) error(0)
+                break
+            } catch (e: Exception) {
+                println("Не верный ввод данных, введите повторно!")
+            }
         }
+        println(agoToText(sum))
     }
 }
 
-fun agoToText(sum: Int) {
-    val totalMinutes = sum * MINUTE
-    val totalHours = sum * MINUTE
-    when {
-        totalMinutes in 0..MINUTE -> {
-            println("Alex")
-            println("был(а) только что")
-        }
-        totalMinutes in MINUTE + 1..HOUR -> {
-            val minutesAgo = totalMinutes / 60
+fun agoToText(sum: Int): String {
+    return when (sum) {
+        in 0..MINUTE -> ("Alex" + "был(а) только что")
+        in MINUTE + 1..HOUR -> {
             println("Вася")
-            print("был(а) в сети $minutesAgo ")
-            minutes(sum)
+            mode(sum)
         }
-        totalHours in HOUR + 1..DAY -> {
-            val hoursAgo = totalHours / 60 / 60
+        in HOUR + 1..DAY -> {
             println("Женя :)")
-            print("был(а) в сети $hoursAgo ")
-            hours(sum)
+            mode(sum)
         }
-        totalHours in DAY + 1..2 * DAY -> {
-            println("Аркадий")
-            println("был(а) в сети сегодня ")
-        }
-        totalHours in 2 * DAY + 1..3 * DAY -> {
-            println("Олег")
-            println("был(а) в сети вчера ")
-        }
-        totalHours > 3 * DAY + 1 -> {
-            println("Вован")
-            println("был(а) в сети давно ")
-        }
+        in DAY + 1..2 * DAY -> ("Аркадий" + "был(а) в сети сегодня")
+        in 2 * DAY + 1..3 * DAY -> ("Олег" + "был(а) в сети вчера")
+        in DAY * 3 + 1..Int.MAX_VALUE -> ("Вован" + "был(а) в сети давно")
         else -> error("Несоблюдены все правила!")
     }
 }
 
-fun minutes(sum: Int) {
-    when {
-        sum % 10 == 1 && sum % 100 != 11 -> println("минуту назад")
-        sum % 10 == 2 && sum % 100 != 12 ||
-                sum % 10 == 3 && sum % 100 != 13 ||
-                sum % 10 == 4 && sum % 100 != 14 -> println("минуты назад")
-        sum < 0 -> println("Не верный ввод данных, введите положительное число!")
-        else -> println("минут назад")
-    }
-}
-
-fun hours(sum: Int) {
-    val sum = sum / 60
-    when {
-        sum % 10 == 1 && sum % 100 != 11 -> println("час назад")
-        sum % 10 == 2 && sum % 100 != 12 ||
-                sum % 10 == 3 && sum % 100 != 13 ||
-                sum % 10 == 4 && sum % 100 != 14 -> println("часа назад")
-        sum < 0 -> println("Не верный ввод данных, введите положительное число!")
-        else -> println("часов назад")
+fun mode(sum: Int): String {
+    val totalMinutes = sum / MINUTE
+    val totalHours = sum / HOUR
+    return when (sum) {
+        in MINUTE + 1..HOUR -> {
+            if (totalMinutes % 10 == 1 && totalMinutes != 11) ("Был(а) $totalMinutes минуту назад")
+            else if (totalMinutes % 10 in 2..4 && totalMinutes !in 12..14) ("Был(а) $totalMinutes минуты назад")
+            else ("Был(а) $totalMinutes минут назад")
+        }
+        in HOUR + 1..DAY -> {
+            if (totalHours % 10 == 1 && totalHours != 11) ("Был(а) $totalHours час назад")
+            else if (totalHours % 10 in 2..4 && totalHours !in 12..14) ("Был(а) $totalHours часа назад")
+            else ("Был(а) $totalHours часов назад")
+        }
+        else -> error("Несоблюдены все правила!")
     }
 }
