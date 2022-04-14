6544bdc5cdded85243ec4f26fa7fa0b522ea765d
ANDru <93010255+AndreyGureev@users.noreply.github.com>
2022-04-14 16:09:46 +0400

    Update WallService.kt
    modification

--- src/main/kotlin/block2/WallService.kt
+++ src/main/kotlin/block2/WallService.kt
@@ -5,42 +5,40 @@ package block2
 object WallService {
     private var posts = emptyArray<Post>()
 
-    fun add(post: Post): Post {
-        val post = post.copy(id = generationId())
+    fun add(wall: Post): Post {
+        val post = wall.copy(id = generationId())
         posts += post
 //        println(posts.last())
         return posts.last()
     }
 
-    fun update(post: Post): Boolean {
-        val postId = post.component1()
-        val postNext = post
+    fun update(wall: Post): Boolean {
         for ((index, post) in posts.withIndex()) {
-            if (post.id == postId) {
+            if (post.id == wall.component1()) {
                 posts[index] = post.copy(
-                    fromId = postNext.fromId,
-                    createdBy = postNext.createdBy,
-                    text = postNext.text,
-                    replyOwnerId = postNext.replyOwnerId,
-                    replyPostId = postNext.replyPostId,
-                    friendsOnly = postNext.friendsOnly,
-                    comments = postNext.comments,
-                    copyright = postNext.copyright,
-                    likes = postNext.likes,
-                    reposts = postNext.reposts,
-                    views = postNext.views,
-                    postType = postNext.postType,
-                    signerId = postNext.signerId,
-                    canPin = postNext.canPin,
-                    canDelete = postNext.canDelete,
-                    canEdit = postNext.canEdit,
-                    isPinned = postNext.isPinned,
-                    markedAsAds = postNext.markedAsAds,
-                    isFavorite = postNext.isFavorite,
-                    postponedId = postNext.postponedId,
-                    postSource = postNext.postSource,
-                    geo = postNext.geo,
-                    copyHistory = postNext.copyHistory,
+                    fromId = wall.fromId,
+                    createdBy = wall.createdBy,
+                    text = wall.text,
+                    replyOwnerId = wall.replyOwnerId,
+                    replyPostId = wall.replyPostId,
+                    friendsOnly = wall.friendsOnly,
+                    comments = wall.comments,
+                    copyright = wall.copyright,
+                    likes = wall.likes,
+                    reposts = wall.reposts,
+                    views = wall.views,
+                    postType = wall.postType,
+                    signerId = wall.signerId,
+                    canPin = wall.canPin,
+                    canDelete = wall.canDelete,
+                    canEdit = wall.canEdit,
+                    isPinned = wall.isPinned,
+                    markedAsAds = wall.markedAsAds,
+                    isFavorite = wall.isFavorite,
+                    postponedId = wall.postponedId,
+                    postSource = wall.postSource,
+                    geo = wall.geo,
+                    copyHistory = wall.copyHistory,
                     attachments = postNext.attachments
                 )
 //                println(posts[index])
