c06ffcbd66c0f4a02730d8eedbe1f146945757e7
ANDru <93010255+AndreyGureev@users.noreply.github.com>
2022-04-17 17:39:24 +0400

    Update and rename PhotoAttachments.kt to Photo.kt
    Update 3.1

---
+++ src/main/kotlin/block2/Attachment/Photo.kt
@@ -0,0 +1,20 @@
+package block2.Attachment
+
+data class Photo(
+    val id: Int,
+    val albumId: Int,
+    val ownerId: Int,
+    val userId: Int,
+    val text: String,
+    val date: Long,
+    val sizes: Array<PhotoCopy>,
+    val width: Int,
+    val height: Int
+)
+
+data class PhotoCopy(
+    val type: String,
+    val url: String,
+    val width: Int,
+    val height: Int
+)
