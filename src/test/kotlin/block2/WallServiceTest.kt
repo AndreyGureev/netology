6354e9c77723f7917ccf2cf0efaffe24ebb50291
ANDru <93010255+AndreyGureev@users.noreply.github.com>
2022-04-17 17:44:43 +0400

    Update WallServiceTest.kt
    Update 3.1

--- src/test/kotlin/block2/WallServiceTest.kt
+++ src/test/kotlin/block2/WallServiceTest.kt
@@ -1,312 +1,76 @@
 package block2
 
-import block2.Attachment.AudioAttachments
-import block2.Attachment.PhotoAttachments
-import block2.Attachment.VideoAttachments
 import org.junit.Test
-
 import org.junit.Assert.*
 
 class WallServiceTest {
 
     @Test
     fun add() {
-        WallService.clearArr()
-        val total = WallService.add(
-            Post(
-                id = 23,
-                555,
-                33,
-                99,
-                20220406,
-                "Создание записи G",
-                12,
-                13,
-                false,
-                Post.Comments(2),
-                Post.Copyright(55, "https://dev.vk.com/reference/objects/post", "name", "type"),
-                Post.Likes(3),
-                Post.Reposts(35436),
-                Post.Views(234345757),
-                PostType.Post,
-                Post.PostSource(Type.Widget, Platform.Iphone, "https://dev.vk.com/reference/objects/post-source"),
-                arrayOf(
-                    AudioAttachments(
-                        AudioAttachments.Audio(
-                            14,
-                            167,
-                            "Rammstein",
-                            "Engel",
-                            265,
-                            "https://www.rammsteinworld.com",
-                            19970401
-                        )
-                    )
-                ),
-                Post.Geo("World", "30°58'51''", null),
-                65,
-                null,
-                donut = Post.Donut(
-                    true,
-                    60,
-                    "placeholder",
-                    false,
-                    null
-                ),
-                postponedId = 555
-            )
-        )
-        assertNotEquals(0, total.id)
+        val service = WallService()
+        val expected = service.getLastPostId() + 1
+        service.add(post = Post())
+        assertEquals(expected, service.getLastPostId())
     }
 
     @Test
-    fun updateTrue() {
-        WallService.clearArr()
-        WallService.add(
-            Post(
-                id = 23,
-                555,
-                33,
-                99,
-                20220406,
-                "Создание записи X",
-                12,
-                13,
-                false,
-                Post.Comments(2),
-                Post.Copyright(55, "https://dev.vk.com/reference/objects/post", "name", "type"),
-                Post.Likes(3),
-                Post.Reposts(35436),
-                Post.Views(234345757),
-                PostType.Post,
-                Post.PostSource(Type.Widget, Platform.Iphone, "https://dev.vk.com/reference/objects/post-source"),
-                arrayOf(
-                    VideoAttachments(
-                        VideoAttachments.Video(
-                            31,
-                            221,
-                            "YouTube",
-                            265,
-                            "https://www.youtube.com/watch?v=x2rQzv8OWEY",
-                            20150715,
-                            676,
-                            138269570,
-                            "GOM"
-                        )
-                    ), PhotoAttachments(
-                        PhotoAttachments.Photo(
-                            234, 24, 77, 9, "Single", 1994
-                        )
-                    )
-                ),
-                Post.Geo("World", "30°58'51''", null),
-                65,
-                null,
-                donut = Post.Donut(
-                    true,
-                    60,
-                    "placeholder",
-                    false,
-                    null
-                ),
-                postponedId = 555
-            )
-        )
-        WallService.add(
-            Post(
-                id = 23,
-                555,
-                33,
-                99,
-                20220406,
-                "Создание записи X",
-                12,
-                13,
-                false,
-                Post.Comments(2),
-                Post.Copyright(55, "https://dev.vk.com/reference/objects/post", "name", "type"),
-                Post.Likes(3),
-                Post.Reposts(35436),
-                Post.Views(234345757),
-                PostType.Post,
-                Post.PostSource(Type.Widget, Platform.Iphone, "https://dev.vk.com/reference/objects/post-source"),
-                arrayOf(
-                    AudioAttachments(
-                        AudioAttachments.Audio(
-                            14,
-                            167,
-                            "Rammstein",
-                            "Engel",
-                            265,
-                            "https://www.rammsteinworld.com",
-                            19970401
-                        )
-                    )
-                ),
-                Post.Geo("World", "30°58'51''", null),
-                65,
-                null,
-                donut = Post.Donut(
-                    true,
-                    60,
-                    "placeholder",
-                    false,
-                    null
-                ),
-                postponedId = 555
-            )
-        )
-        val total = WallService.update(
-            Post(
-                id = 1,
-                555,
-                33,
-                99,
-                20220406,
-                "Создание записи X",
-                12,
-                13,
-                false,
-                Post.Comments(2),
-                Post.Copyright(55, "https://dev.vk.com/reference/objects/post", "name", "type"),
-                Post.Likes(3),
-                Post.Reposts(35436),
-                Post.Views(234345757),
-                PostType.Post,
-                Post.PostSource(Type.Widget, Platform.Iphone, "https://dev.vk.com/reference/objects/post-source"),
-                arrayOf(
-                    VideoAttachments(
-                        VideoAttachments.Video(
-                            31,
-                            221,
-                            "YouTube",
-                            265,
-                            "https://www.youtube.com/watch?v=x2rQzv8OWEY",
-                            20150715,
-                            676,
-                            138269570,
-                            "GOM"
-                        )
-                    ), PhotoAttachments(
-                        PhotoAttachments.Photo(
-                            234, 24, 77, 9, "Single", 1994
-                        )
-                    )
-                ),
-                Post.Geo("World", "30°58'51''", null),
-                65,
-                null,
-                donut = Post.Donut(
-                    true,
-                    60,
-                    "placeholder",
-                    false,
-                    null
-                ),
-                postponedId = 555
-            )
-        )
-        assertTrue(total)
+    fun updateExisting() {
+        val service = WallService()
+        service.add(Post(text = "Текст 1"))
+        service.add(Post(text = "Текст 2"))
+        service.add(Post(text = "Текст 3"))
+
+        val updatePost = Post(3, text = "Текст 3 обновлен")
+        val result = service.update(updatePost)
+        assertTrue(result)
+
     }
 
     @Test
-    fun updateFalse() {
-        WallService.clearArr()
-        WallService.add(
-            Post(
-                id = 23,
-                555,
-                33,
-                99,
-                20220406,
-                "Создание записи X",
-                12,
-                13,
-                false,
-                Post.Comments(2),
-                Post.Copyright(55, "https://dev.vk.com/reference/objects/post", "name", "type"),
-                Post.Likes(3),
-                Post.Reposts(35436),
-                Post.Views(234345757),
-                PostType.Post,
-                Post.PostSource(Type.Widget, Platform.Iphone, "https://dev.vk.com/reference/objects/post-source"),
-                arrayOf(
-                    AudioAttachments(
-                        AudioAttachments.Audio(
-                            14,
-                            167,
-                            "Rammstein",
-                            "Engel",
-                            265,
-                            "https://www.rammsteinworld.com",
-                            19970401
-                        )
-                    )
-                ),
-                Post.Geo("World", "30°58'51''", null),
-                65,
-                null,
-                donut = Post.Donut(
-                    true,
-                    60,
-                    "placeholder",
-                    false,
-                    null
-                ),
-                postponedId = 555
-            )
+    fun updateNotExisting() {
+        val service = WallService()
+        service.add(Post(text = "Текст 1"))
+        service.add(Post(text = "Текст 2"))
+        service.add(Post(text = "Текст 3"))
+
+        val updatePost = Post(5, text = "Текст 3 обновлен")
+        val result = service.update(updatePost)
+        assertTrue(!result)
+    }
+
+    @Test
+    fun createComment() {
+        val service = WallService()
+        val post = Post(text = "Comment")
+        service.add(post)
+
+        val comment = Comment(
+            replyToComment = service.getLastPostId(),
+            message = "comment",
+            ownerId = 1,
+            postId = 1,
+            attachments = arrayOf(CommentAttachment("att", 1, 1))
         )
 
-        val total = WallService.update(
-            Post(
-                id = 23,
-                555,
-                33,
-                99,
-                20220406,
-                "Создание записи X",
-                12,
-                13,
-                false,
-                Post.Comments(2),
-                Post.Copyright(55, "https://dev.vk.com/reference/objects/post", "name", "type"),
-                Post.Likes(3),
-                Post.Reposts(35436),
-                Post.Views(234345757),
-                PostType.Post,
-                Post.PostSource(Type.Widget, Platform.Iphone, "https://dev.vk.com/reference/objects/post-source"),
-                arrayOf(
-                    VideoAttachments(
-                        VideoAttachments.Video(
-                            31,
-                            221,
-                            "YouTube",
-                            265,
-                            "https://www.youtube.com/watch?v=x2rQzv8OWEY",
-                            20150715,
-                            676,
-                            138269570,
-                            "GOM"
-                        )
-                    ), PhotoAttachments(
-                        PhotoAttachments.Photo(
-                            234, 24, 77, 9, "Single", 1994
-                        )
-                    )
-                ),
-                Post.Geo("World", "30°58'51''", null),
-                65,
-                null,
-                donut = Post.Donut(
-                    true,
-                    60,
-                    "placeholder",
-                    false,
-                    null
-                ),
-                postponedId = 555
-            )
+        service.createComment(comment)
+        val result = service.findPostById(service.getLastPostId())?.let { service.getPostComments(it) }
+
+
+        assertEquals("comment", result)
+    }
+
+    @Test
+    fun shouldThrow() {
+        val service = WallService()
+        val post = Post(text = "Comment")
+        service.add(post)
+
+        val comment = Comment(
+            replyToComment = service.getLastPostId(), message = "comment",
+            ownerId = 1,
+            postId = 2,
+            attachments = arrayOf(CommentAttachment("att", 1, 1))
         )
-        assertFalse(total)
+        service.createComment(comment)
     }
 }
