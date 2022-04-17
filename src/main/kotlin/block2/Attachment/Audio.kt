a9a6670a5037216980f38f4e74de68cc912362a2
ANDru <93010255+AndreyGureev@users.noreply.github.com>
2022-04-17 17:37:56 +0400

    Update and rename AudioAttachments.kt to Audio.kt
    Update 3.1

---
+++ src/main/kotlin/block2/Attachment/Audio.kt
@@ -0,0 +1,16 @@
+package block2.Attachment
+
+data class Audio(
+    val id: Int,
+    val ownerId: Int,
+    val artist: String,
+    val title: String,
+    val duration: Int,
+    val url: String,
+    val lyricsId: Int,
+    val albumId: Int,
+    val genreId: Int,
+    val date: Long,
+    val noSearch: Boolean,
+    val isHq: Boolean
+)
