941ff92a25fd3ccbc88b77eaac6921af98e45189
ANDru <93010255+AndreyGureev@users.noreply.github.com>
2022-04-18 20:47:42 +0400

    Add files via upload
    Added 3.2.Generics-Collections

---
+++ src/main/kotlin/block3/3.2.Generics-Collections.kt
@@ -0,0 +1,70 @@
+package block3
+
+// «3.2. Generics и коллекции»
+// Мини-проект - NoteService
+fun main() {
+    NoteService.addNote(Note(title = "Добрый день", text = "Good day"))
+    NoteService.addNote(Note(title = "Добрый вечер", text = "Good evening"))
+    println(NoteService.getNoteWithComments(NoteService.getNoteById(1)))
+    println(NoteService.getNoteWithComments(NoteService.getNoteById(2)))
+    NoteService.addComment(Comment(text = "комментарий к записи 2"), NoteService.getNoteById(2))
+
+    NoteService.editNote(
+        NoteService.getNoteById(1), "Заголовок 1 отредактирован",
+        "Первая запись исправлена"
+    )
+    NoteService.editNote(
+        NoteService.getNoteById(2), "Заголовок 2 отредактирован",
+        "2я запись исправлена"
+    )
+
+    NoteService.addComment(
+        Comment(text = "комментарий к комментарию"),
+        NoteService.getNoteById(2).getCommentById(1)
+    )
+    NoteService.addComment(
+        Comment(text = "еще один комментарий к комментарию"),
+        NoteService.getNoteById(2).getCommentById(1)
+    )
+    NoteService.addComment(
+        Comment(text = "и еще один комментарий к комментарию"),
+        NoteService.getNoteById(2).getCommentById(1)
+    )
+
+    NoteService.addComment(
+        Comment(text = "комментарий к комментарию к комментарию"),
+        NoteService.getNoteById(2).getCommentById(1).getCommentById(1)
+    )
+
+    NoteService.editComment(
+        NoteService.getNoteById(2).getCommentById(1),
+        "комментарий к записи 2 отредактирован"
+    )
+    NoteService.editComment(
+        NoteService.getNoteById(2).getCommentById(1).getCommentById(1)
+            .getCommentById(1), "комментарий к комментарию к комментарию отредактирован"
+    )
+
+    println("-----После добавления и редактирования комментариев-----")
+
+    println(NoteService.getNoteWithComments(NoteService.getNoteById(1)))
+    println(NoteService.getNoteWithComments(NoteService.getNoteById(2)))
+
+    NoteService.deleteNote(2)
+
+    println("-----После удаления записи 2-----")
+
+    println(NoteService.getNoteWithComments(NoteService.getNoteById(1)))
+    try {
+        println(NoteService.getNoteWithComments(NoteService.getNoteById(2)))
+    } catch (e: NoteIdNotFoundException) {
+        println("Запись 2 отсутствует\n")
+    }
+
+    NoteService.restoreNote(2)
+
+    println("-----После восстановления записи 2-----")
+
+    println(NoteService.getNoteWithComments(NoteService.getNoteById(1)))
+    println(NoteService.getNoteWithComments(NoteService.getNoteById(2)))
+}
---
+++ src/main/kotlin/block3/Notable.kt
@@ -0,0 +1,9 @@
+package block3
+
+interface Notable<D> {
+    fun addTo(dest: MutableList<D>)
+    fun delete()
+    fun restore()
+    fun getCommentById(id: Int): Comment
+    fun getCommentByIdForced(id: Int): Comment
+}
