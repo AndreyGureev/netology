b777ed6ed1f213358d51200a7970dfde51c23cca
ANDru <93010255+AndreyGureev@users.noreply.github.com>
2022-04-18 21:49:27 +0400

    Delete src/main/kotlin/block3 directory
    Remove

--- src/main/kotlin/block3/Notable.kt
+++
@@ -1,9 +0,0 @@
-package block3
-
-interface Notable<D> {
-    fun addTo(dest: MutableList<D>)
-    fun delete()
-    fun restore()
-    fun getCommentById(id: Int): Comment
-    fun getCommentByIdForced(id: Int): Comment
-}
